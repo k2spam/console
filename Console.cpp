b1b6f2c06bc7fd0d6edef69f6a1752345674eb42
Vladimir Khlebnikov <v.khlebnikov@angaratech.ru>
2022-08-19 20:27:05 +0300

    Homework 14: first & last letter fix

--- Console.cpp
+++ Console.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-    std::string str;
+	std::string str;
 
-    std::cout << "Insert string: ";
-    std::getline (std::cin, str);
+	std::cout << "Insert string: ";
+	std::getline(std::cin, str);
 
-    std::cout << "\nResults:\n";
-    std::cout << "string: " << str << '\n';
-    std::cout << "length: " << str.length() << '\n';
-    std::cout << "first char: " << str[0] << '\n';
-    std::cout << "last char: " << str[str.length() - 1] << '\n';
+	std::cout << "\nResults:\n";
+	std::cout << "string: " << str << '\n';
+	std::cout << "length: " << str.length() << '\n';
+	std::cout << "first char: " << str.front() << '\n';
+	std::cout << "last char: " << str.back() << '\n';
 }

61ec0c055e7e86a9d9fe972bc6def4b61502e1f7
Vladimir Khlebnikov <v.khlebnikov@angaratech.ru>
2022-08-20 18:18:01 +0300

    Homework 15: even and odd numbers

--- Console.cpp
+++ Console.cpp
@@ -1,16 +1,40 @@
 ﻿#include <iostream>
-#include <string>
+
+void print(int n)
+{
+    std::cout << n << '\n';
+}
+
+/*
+* type = 0 all numbers
+* type = 1 odd numbers
+* type = 2 even numbers
+*/
+void allEvenOdd(int min = 0, int max = 100, int type = 0)
+{
+    std::cout << "Results:\n";
+
+    for (int i = min; i <= max; i++)
+        switch (type)
+        {
+        case 0:
+            print(i);
+            break;
+        case 1:
+            if (i % 2 != 0) print(i);
+            break;
+        case 2:
+            if (i % 2 == 0) print(i);
+            break;
+        default:
+            print(i);
+            break;
+        }
+}
 
 int main()
 {
-	std::string str;
-
-	std::cout << "Insert string: ";
-	std::getline(std::cin, str);
-
-	std::cout << "\nResults:\n";
-	std::cout << "string: " << str << '\n';
-	std::cout << "length: " << str.length() << '\n';
-	std::cout << "first char: " << str.front() << '\n';
-	std::cout << "last char: " << str.back() << '\n';
+    allEvenOdd(0, 12, 0);
+    allEvenOdd(0, 12, 1);
+    allEvenOdd(0, 12, 2);
 }

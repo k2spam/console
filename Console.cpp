d64ef2b146e4672049915a1f89b2a0a9b75e2fc1
Vladimir Khlebnikov <v.khlebnikov@angaratech.ru>
2022-08-22 03:06:40 +0300

    Homework 16: arrays

--- Console.cpp
+++ Console.cpp
@@ -1,40 +1,32 @@
 ﻿#include <iostream>
-
-void print(int n)
-{
-    std::cout << n << '\n';
-}
-
-/*
-* type = 0 all numbers
-* type = 1 odd numbers
-* type = 2 even numbers
-*/
-void allEvenOdd(int min = 0, int max = 100, int type = 0)
-{
-    std::cout << "Results:\n";
-
-    for (int i = min; i <= max; i++)
-        switch (type)
-        {
-        case 0:
-            print(i);
-            break;
-        case 1:
-            if (i % 2 != 0) print(i);
-            break;
-        case 2:
-            if (i % 2 == 0) print(i);
-            break;
-        default:
-            print(i);
-            break;
-        }
-}
+#include <time.h>
 
 int main()
 {
-    allEvenOdd(0, 12, 0);
-    allEvenOdd(0, 12, 1);
-    allEvenOdd(0, 12, 2);
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+
+	const int n = 5;
+	int arr[n][n];
+
+	std::cout << "Generated array:\n";
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = i + j;
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+
+	int sum = 0;
+	int lineNumber = buf.tm_mday % n;
+
+	for (int num : arr[lineNumber])
+		sum += num;
+
+	std::cout << "Line number: " << lineNumber << '\n';
+	std::cout << "Line sum: " << sum << '\n';
 }

dd5965b2352d4ca37ac84074325438ae5a3c3ab2
Vladimir Khlebnikov <v.khlebnikov@angaratech.ru>
2022-09-06 00:53:34 +0300

    Homework 18: pointer update

--- Console.cpp
+++ Console.cpp
@@ -1,41 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
 
+template<class T>
 class Stack
 {
-private:
+private: 
 	int size = 0;
-	int* pInt = new int[size];
+	T* pArray = new T[size];
 
 public:
-	/*~Stack()
-	{
-		delete pInt;
-	}*/
-	void Push(int val)
+	void Push(T val)
 	{
 		size += 1;
-		*(pInt + size - 1) = val;
+		*(pArray + size - 1) = val;
 	}
 
-	int Pop()
+	T Pop()
 	{
-		return size> 0 ? *(pInt + size - 1) : 0;
+		return *(pArray + size - 1);
 	}
 };
 
 int main()
 {
-	Stack* arr = new Stack;
-	
-	arr->Push(11);
-	cout << "First push \t" << arr->Pop() << '\n';
-	
-	arr->Push(22);
-	cout << "Second push \t" << arr->Pop() << '\n';
+	Stack<int>* intArray = new Stack<int>;
+	intArray->Push(11);
+	cout << "Int Array \t" << intArray->Pop() << '\n';
+	delete intArray;
 
-	arr->Push(33);
-	cout << "Third push \t" << arr->Pop() << '\n';
+	Stack<float>* floatArray = new Stack<float>;
+	floatArray->Push(1.1f);
+	floatArray->Push(2.2f);
+	cout << "Float Array \t" << floatArray->Pop() << '\n';
+	delete floatArray;
 
-	delete arr;
+	Stack<double>* doubleArray = new Stack<double>;
+	doubleArray->Push(.11);
+	doubleArray->Push(.22);
+	doubleArray->Push(.33);
+	cout << "Double Array \t" << doubleArray->Pop() << '\n';
+	delete doubleArray;
 }
